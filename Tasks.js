3a4f047da82df30d6eb797acf901079deb1e78e3
Shivam Garg <gargshivam963963@gmail.com>
2023-02-01 09:49:58 +0530

    object practice main

---
+++ Tasks.js
@@ -0,0 +1,19 @@
+// Write the code, one line for each action:
+
+// Create an empty object user.
+// Add the property name with the value John.
+// Add the property surname with the value Smith.
+// Change the value of the name to Pete.
+// Remove the property name from the object.
+
+const user = {};
+user.name = "John"; //add name: value as a user property
+// console.log(user);
+
+user.surname = "Smith";
+user.name = "Pete";
+// delete user.name;
+console.log(user);
+
+// output:
+// { name: 'Pete', surname: 'Smith' }
